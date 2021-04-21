0519a08c6812f1940494db0d067b33bcd09a5f0f
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-04-21 13:22:01 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -353,7 +353,7 @@ def Masha_about_callback(update: Update, context: CallbackContext):
                  ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
-                 \n_Masha's licensed under the GNU General Public License v3.0_
+                 \n_esther's licensed under the GNU General Public License v3.0_
                  .
                  If you have any question about Masha, let us know at @rosebakthan.""",
             parse_mode=ParseMode.MARKDOWN,
