0c7c91711e063ba34e3ee77621f28e71065cfabc
Wonder wolf <63580147+dqanshi@users.noreply.github.com>
2021-05-04 00:19:50 +0530

    Update __init__.py

--- MashaRoBot/__init__.py
+++ MashaRoBot/__init__.py
@@ -80,7 +80,7 @@ if ENV:
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", None))
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
